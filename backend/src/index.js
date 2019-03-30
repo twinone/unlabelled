90a28585087fc053a4e3303901d8936ec0a2ffd7
Luuk W. (Twinone) <twinonedevs@gmail.com>
2019-03-30 21:26:56 +0100

    Add CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,6 +38,13 @@ app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})) // support encoded bodies
 
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+
 /// USER REGISTRATION
 
 app.get('/register/:email/:pass', (req, res) => {
