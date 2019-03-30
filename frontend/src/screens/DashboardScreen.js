f195adaec11696667ec54a5a1bc9dbcfe7afd4a0
Jovan Kanevche <kanevche.jovan@gmail.com>
2019-03-31 00:42:27 +0100

    Add AcceptedOrder and IncomingOrder components

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import {
   Button,
   Card,
@@ -9,12 +9,12 @@ import {
 } from '@material-ui/core'
 import AuthContext from '../globals/useToken'
 import FoodTypes from '../components/FoodTypes'
-import ProgressIndicator from '../components/ProgressIndicator'
+//import ProgressIndicator from '../components/ProgressIndicator'
 
 const FOOD_TYPES = ['Burger', 'Kebab', 'Pizza']
 const orders = [
   {
-    name: 'Food ORDER',
+    name: 'Pizza ORDER',
     picture:
       'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
     deliverTime: '30 minutes'
@@ -60,11 +60,63 @@ const Order = ({ name, picture, deliverTime }) => (
   </Card>
 )
 
+const IncomingOrder = ({ name, picture, deliverTime }) => (
+  <Card style={{ flexDirection: 'column' }}>
+    <CardMedia
+      component="img"
+      alt="Contemplative Reptile"
+      height="140px"
+      image={picture}
+      title="Contemplative Reptile"
+    />
+    <CardContent style={{ flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ fontSize: 23, paddingBottom: 8 }}>{name}</div>
+      <div style={{ paddingBottom: 8 }}>Delivery {deliverTime}</div>
+      <div style={{ flexDirection: 'row' }}>
+        <Button style={{ flex: 1 }}>Accept</Button>
+        <Button style={{ flex: 1 }}>Decline</Button>
+      </div>
+    </CardContent>
+  </Card>
+)
+
+const AcceptedOrder = ({ name, picture, deliverTime }) => (
+  <Card style={{ flexDirection: 'column' }}>
+    <CardMedia
+      component="img"
+      alt="Contemplative Reptile"
+      height="140px"
+      image={picture}
+      title="Contemplative Reptile"
+    />
+    <CardContent
+      style={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingBottom: '0px !important'
+      }}
+    >
+      <div style={{ fontSize: 23, paddingBottom: 8 }}>{name}</div>
+      <div style={{ paddingBottom: 8 }}>Delivery {deliverTime}</div>
+
+      <Button>Ship it</Button>
+    </CardContent>
+  </Card>
+)
+
 function DashboardScreen() {
   const name = 'Restaurant'
   const types = ['Burger', 'Kebab']
-  const [{ restaurantName, token }, setAuth] = useContext(AuthContext)
+  const [{ restaurantName }, setAuth] = useContext(AuthContext)
   const onDelete = () => {}
+
+  useEffect(() => {
+    const socket = new WebSocket('/ws')
+    console.log(socket)
+    socket.addEventListener('message', e => {
+      console.log(e)
+    })
+  }, [])
 
   return (
     <div
@@ -82,16 +134,28 @@ function DashboardScreen() {
         currentTypes={['Kebab', 'Pizza']}
         setTypes={() => {}}
       />
-      <ProgressIndicator />
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '200px 200px 200px',
-          gridColumnGap: '10px',
-          gridRowGap: '10px'
-        }}
-      >
-        {orders.map(Order)}
+      <div style={{ flexDirection: 'row' }}>
+        <div style={styles.gridContainer}>
+          <h4>Incoming orders</h4>
+          <div
+            style={{
+              paddingRight: 89,
+              ...styles.grid
+            }}
+          >
+            {orders.map(IncomingOrder)}
+          </div>
+        </div>
+        <div style={{ flexDirection: 'column' }}>
+          <h4>Accepted Orders</h4>
+          <div
+            style={{
+              ...styles.grid
+            }}
+          >
+            {orders.map(AcceptedOrder)}
+          </div>
+        </div>
       </div>
       <Button
         onClick={() => {
@@ -105,6 +169,20 @@ function DashboardScreen() {
       </Button>
     </div>
   )
+}
+
+const styles = {
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: '200px 200px',
+    gridColumnGap: '10px',
+    gridRowGap: '10px'
+  },
+  gridContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }
 }
 
 export default DashboardScreen
