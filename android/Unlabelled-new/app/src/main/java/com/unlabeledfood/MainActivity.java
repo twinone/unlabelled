7138933368eff7e24c4713320194249d0639db68
Luuk W. (Twinone) <twinonedevs@gmail.com>
2019-03-30 23:12:12 +0100

    Add stuff

--- android/Unlabelled-new/app/src/main/java/com/unlabeledfood/MainActivity.java
+++ android/Unlabelled-new/app/src/main/java/com/unlabeledfood/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity
         } catch (InstantiationException e) {
             e.printStackTrace();
         }
-
     }
 
 
--- android/Unlabelled-new/app/src/main/java/com/unlabeledfood/UserFragment.java
+++ android/Unlabelled-new/app/src/main/java/com/unlabeledfood/UserFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,13 @@ public class UserFragment extends Fragment {
     }
 
     private void onPizzaClick() {
-        Log.d("Pizza", "Pizza");
+        getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, new PizzaIngredientsFragment())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .addToBackStack(null)
+                .commit();
+
     }
 }
