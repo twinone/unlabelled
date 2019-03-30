54ed8f25836fc987c894999454ac5e7df3222c73
Luuk W. (Twinone) <twinonedevs@gmail.com>
2019-03-30 22:49:04 +0100

    Restart app, add selector screen in UserFragment

---
+++ android/Unlabelled-new/app/src/main/java/com/unlabeledfood/UserFragment.java
@@ -0,0 +1,33 @@
+package com.unlabeledfood;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_user, container, false);
+        v.findViewById(R.id.pizza).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPizzaClick();
+            }
+        });
+
+        return v;
+
+    }
+
+    private void onPizzaClick() {
+        Log.d("Pizza", "Pizza");
+    }
+}
